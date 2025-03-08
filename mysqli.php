548880751751521771a278d768835ab88c92303b
EliOrozco <92065409+EliOrozco@users.noreply.github.com>
2025-03-08 15:15:05 -0600

    cambios para funcionar en x10hosting
    yeah

---
+++ mysqli.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<head>
+    <title>Menú para las tareas de PHP INTRO</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
+</head>
+<html>
+    <body class="index-page">
+        <ul>
+            <li><a href="holamundo.php">holamundo</a></li>
+            <li><a href="variables.php">variables</a></li>
+            <li><a href="operadores.php">operadores</a></li>
+            <li><a href="estructuras.php">estructuras</a></li>
+            <li><a href="funciones.php">funciones</a></li>
+            <li><a href="array.php">array</a></li>
+            <li><a href="formulario.php">formulario</a></li>
+            <li><a href="mysqli.php">msqli</a></li>
+        </ul>
+        <h1>CONTENIDO</h1>
+        <?php
+            $servername = "localhost";
+            $username = "wekccrjt_mi_base_de_datos";
+            $password = "ebSZzSTab2wPBE62AZCE";
+            $dbname = "wekccrjt_mi_base_de_datos";
+
+            //crear conexion
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            //verificar conexion
+            if ($conn->connect_error) {
+                die("Conexión fallida: " . $conn->connect_error);
+            }
+            
+            // Consulta SQL
+            $sql = "SELECT id, nombre, edad FROM usuarios";
+            $result = $conn->query($sql);
+            
+            if ($result->num_rows > 0) {
+                // Mostrar datos de cada fila
+                while($row = $result->fetch_assoc()) {
+                    echo "id: " . $row["id"]. " - Nombre: " . $row["nombre"]. " - Edad: " . $row["edad"]. "<br>";
+                }
+            } else {
+                echo "0 resultados";
+            }
+            
+            $conn->close();
+        ?>
+    </body>
+</html>
---
+++ procesar.php
@@ -0,0 +1,6 @@
+<?php
+$nombre = $_POST['nombre'];
+$edad = $_POST['edad'];
+
+echo "NOMBRE: $nombre, edad: $edad";
+?>
